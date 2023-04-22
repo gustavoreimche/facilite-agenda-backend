28aa9809eeeb879eec2efff5eec4328aa42b7186
Gustavo Reimche <gustavoreimche@hotmail.com>
2023-04-22 14:11:21 -0300

    teste

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/users', userRoutes)
+app.use('/', userRoutes)
 app.use('/agendamentos', agendamentoRoutes)
 app.use('/despesas', despesasRoutes)
 
--- src/routes/agendamentoRoutes.ts
+++ src/routes/agendamentoRoutes.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import * as AgendamentoController from '../controllers/agendamentoController'
-import * as UserController from '../controllers/userController'
 
 const router = Router();
 
@@ -9,7 +8,6 @@ router.post('/create', AgendamentoController.createAgendamento)
 router.put('/update/:id', AgendamentoController.updateAgendamento)
 
 router.delete('/delete/:id', AgendamentoController.deleteAgendamento)
-router.get('/', UserController.getUsers)
 
 router.get('/:idUser', AgendamentoController.getByIdUser)
 
