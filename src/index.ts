6625df337ec87ce1bb997f51b9be93d960636782
Gustavo Reimche <gustavoreimche@hotmail.com>
2023-04-22 10:28:01 -0300

    teste

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,11 @@ require('./database/mongo')
 const app = express();
 
 app.use(express.json());
-
 app.use(cors());
 
 app.use('/agendamentos', agendamentoRoutes)
 app.use('/despesas', despesasRoutes)
 app.use('/users', userRoutes)
+app.get('/')
 
 app.listen(3000)
