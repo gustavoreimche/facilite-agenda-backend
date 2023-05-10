e9b04f1de25f57b1d574a448239484ef49bd9c71
Gustavo Reimche <gustavoreimche@hotmail.com>
2023-05-10 12:59:42 -0300

    update create user

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import * as UserController from '../controllers/userController'
+import * as UserController from "../controllers/userController";
 import { auth } from "../middlewares/auth";
 
 const router = Router();
 
 router.get("/", auth.private, UserController.getUsers);
-router.get("/email/:email", auth.private, UserController.getUsersByEmail);
-router.post("/create", auth.private, UserController.createUser);
-router.post('/verificaacesso', UserController.getUserByEmailAndPassword)
+router.get("/email/:email", UserController.getUsersByEmail);
+router.post("/create", UserController.createUser);
+router.post("/verificaacesso", UserController.getUserByEmailAndPassword);
 router.get("/:id", auth.private, UserController.getUserById);
 
 export default router;
